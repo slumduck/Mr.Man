dca4ad0bb69caeae23e10d23038d8b037f255877
slumduck <334176612@qq.com>
2016-08-23 11:36:48 +0800

    补充提交

---
+++ bookmark/bookmark-dao/src/test/java/com/zl/bookmark/dao/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.zl.bookmark.dao;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.zl.bookmark.dao.repository.UserRepository;
+
+/** 
+ * @author wt0911 zl  
+ */
+public class UserRepositoryTest {
+
+	@Resource
+	private UserRepository userRepositoryImpl;
+	@Test
+	public void test(){
+		//userRepositoryImpl.;
+	}
+}
---
+++ bookmark/bookmark-web/src/main/webapp/WEB-INF/Readme.txt
@@ -0,0 +1,2 @@
+对于springMVC 中配置根"/"目录的一些理解
+由于使用了freemarker并且配置的
